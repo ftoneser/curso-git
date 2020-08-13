2968187f42001adbc149760455a39035ae0e3048
Fernando Toneser <fernando@tds.com.br>
2020-08-13 16:56:37 -0300

    acrescentando tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,6 +6,7 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=================");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como voce esta hoje");
             Console.WriteLine("Tenha um boa dia");
